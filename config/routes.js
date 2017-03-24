5f97871b825f8f489f314de367b7919f655a5e05
omaotzu <omarhphillips@gmail.com>
2017-03-24 11:26:17 +0000

    server running throwing err due to lack of custom responses - working as expected

---
+++ config/routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+
+router.all('/*', (req, res) => res.notFound());
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
-const { port } = require('./config/environment');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// mongoose.plugin(require('./lib/globalToJSON'));
+mongoose.Promise = require('bluebird');
+const { port, env, dbURI } = require('./config/environment');
+const routes = require('./config/routes');
+// const customResponses = require('./lib/customResponses');
+// const errorHandler = require('./lib/errorHandler');
 
 const app = express();
 
+mongoose.connect(dbURI);
+
+if(env !== 'test') app.use(morgan('dev'));
+
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json({limit: '5mb'}));
+
+// app.use(customResponses);
+
+app.use('/', routes);
+
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+// app.use(errorHandler);
 
 app.listen(port, () => console.log(`Express is listening on port ${port}`));
