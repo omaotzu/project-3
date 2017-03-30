2e9c602ccde6777e3ad37a2e15f8bcf2690b310b
mrkjdvs <mjd814@gmail.com>
2017-03-30 18:13:02 +0100

    added secure route on EEEEVVVVEEERRRYYYTTTHHHIIINNNGG

--- config/routes.js
+++ config/routes.js
@@ -9,44 +9,54 @@ const secureRoute = require('../lib/secureRoute');
 const zooplas = require('../controllers/zooplas');
 
 router.route('/properties')
+  .all(secureRoute)
   .get(zooplas.properties);
 
 router.route('/properties/:listingId')
+  .all(secureRoute)
   .get(zooplas.selectedProp);
 
 router.route('/users')
   .get(users.index);
 
 router.route('/users/:id')
+  .all(secureRoute)
   .get(users.show)
   .put(imageUpload, users.update)
   .delete(users.delete);
 
 router.route('/groups')
+  .all(secureRoute)
   .get(groups.index)
   .post(groups.create);
 
 router.route('/groups/:id')
+  .all(secureRoute)
   .get(groups.show)
   // .put(groups.update)
   .delete(groups.delete)
   .post(secureRoute, groups.addProperty);
 
 router.route('/groups/:id/users')
+  .all(secureRoute)
   .put(groups.addUser);
 
 router.route('/groups/:id/users/:userId')
+  .all(secureRoute)
   .delete(groups.deleteUser);
 
 router.route('/groups/:id/properties')
+  .all(secureRoute)
   .get(zooplas.selectedProp);
 
 router.route('/crimes')
+  .all(secureRoute)
   .get(crimes.getCrimes);
 
 router.route('/groups/:id/properties/:listingId')
+  .all(secureRoute)
   .get(zooplas.selectedProp)
-  .delete(secureRoute, groups.deleteProperty);
+  .delete(groups.deleteProperty);
 
 router.route('/groups/:id/properties/:listingId/notes')
   .post(secureRoute, groups.addNote);
